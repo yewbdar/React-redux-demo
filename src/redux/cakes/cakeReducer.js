8bcf42eb0a00e181d23fb312b42b2ac9146f781e
yewbdar <yewbdar.grima@Gmail.com>
2019-10-17 12:21:18 -0500

    react Redux

---
+++ src/redux/cakes/cakeReducer.js
@@ -0,0 +1,16 @@
+import {BUY_CAKE} from './cakeType'
+const intialState={
+    numOfCake:10
+}
+
+const reducer = (state = intialState,action)=>{
+     switch(action.type){
+        case BUY_CAKE:{
+            return{
+                ...state,
+                numOfCake:state.numOfCake - 1
+            }
+        }
+     }
+}
+export default reducer
